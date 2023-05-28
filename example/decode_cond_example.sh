72b070e9319a4da3056bcb8dc4e1e787d97d2437
Yingce Xia <yingce.xia@gmail.com>
2023-05-29 00:06:00 +0800

    Update decode_cond_example.sh

--- example/decode_cond_example.sh
+++ example/decode_cond_example.sh
@@ -3,7 +3,7 @@ export CUDA_VISIBLE_DEVICES=2
 for seed in {1..2}; do
 for beta in "0.1" "1"; do
 
-bash scripts/generate.sh --beam 20 --seed $seed -D gpcr-bin-cond --dataset GPCR --ckpt checkpoints/crossdock_pdb_A10/checkpoint_best.pt --savedir ./gpcr-outs-condition --beta $beta --gen-vae
+bash ../scripts/generate.sh --beam 20 --seed $seed -D gpcr-bin-cond --dataset GPCR --ckpt checkpoints/crossdock_pdb_A10/checkpoint_best.pt --savedir ./gpcr-outs-condition --beta $beta --gen-vae
 
 done
 done
