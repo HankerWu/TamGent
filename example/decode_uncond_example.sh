559e03380146f1fc6307f88f28bf495cb7d94b54
Yingce Xia <yingce.xia@gmail.com>
2023-05-29 00:04:43 +0800

    Create decode_uncond_example.sh

---
+++ example/decode_uncond_example.sh
@@ -0,0 +1,10 @@
+export CUDA_VISIBLE_DEVICES=2
+
+for seed in {1..5}; do
+for beta in "0.1" "1"; do
+
+bash scripts/generate.sh --beam 20 --seed $seed -D gpcr-bin --dataset GPCR --ckpt checkpoints/crossdock_pdb_A10/checkpoint_best.pt --savedir ./gpcr-outs-uncondition --beta $beta
+
+
+done
+done
